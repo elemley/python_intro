6a4d8ffcd81385a3532e34a8604e68c777bb54ea
Evan Lemley <elemley@uco.edu>
2018-08-20 15:09:10 -0500

    Initial commit

---
+++ python_intro_1.py
@@ -0,0 +1,55 @@
+#This is a comment
+
+#The following is the import section
+#This brings in other python files that have pre-defined functions in them
+#This will be present in every single program you write
+from math import *
+import time
+import numpy as np
+
+#Here's how you can comment out multiple lines
+"""
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import colors
+from psm_plot import *
+from random import *
+from time_step_scrolling import *
+"""
+
+#The following is a sort of "global" area this code will execute first and will always execute
+
+print("test")
+
+#Below is a function definition
+def main():
+    #Notice the indentations... these are used in python rather than things like { }
+    print("test")
+    #Note the above line may or may not be executed
+
+    #define a variable
+
+    #do a calculation
+
+    #do some logic
+
+    #do a loop
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
